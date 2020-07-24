fb0f4eebc444e75dbf90e8a97b57465c2bd9a885
Sofin <sofinwadhwaniya@gmail.com>
2020-07-24 16:58:18 +0530

    D

--- MyWebApp/urls.py
+++ MyWebApp/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-	path('/', home,name='home'),
+	path('', views.home,name='home'),
 	path('contact', contact,name='contact'),
 ]
 if settings.DEBUG:
